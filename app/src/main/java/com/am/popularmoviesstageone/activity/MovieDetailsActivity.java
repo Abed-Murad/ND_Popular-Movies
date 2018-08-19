5d928075d56a1d4e9b21a15e9f85a52774ea0b80
Abdallah.Murad <Abdallah.Murad@mail.com>
2018-08-20 01:22:18 +0300

    add change type icon

--- app/src/main/java/com/am/popularmoviesstageone/activity/MovieDetailsActivity.java
+++ app/src/main/java/com/am/popularmoviesstageone/activity/MovieDetailsActivity.java
@@ -9,23 +9,20 @@ import android.view.View;
 
 import com.am.popularmoviesstageone.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MovieDetailsActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
