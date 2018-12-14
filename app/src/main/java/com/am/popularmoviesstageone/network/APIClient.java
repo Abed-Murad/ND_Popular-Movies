a73bb53c5f8ec371ff0d59f6db2abbb830486efd
Abed Murad <abdallah.Murad@mail.com>
2018-12-14 03:12:40 +0100

    Add Api Key to Every Request

--- app/src/main/java/com/am/popularmoviesstageone/network/APIClient.java
+++ app/src/main/java/com/am/popularmoviesstageone/network/APIClient.java
@@ -1,9 +1,17 @@
 package com.am.popularmoviesstageone.network;
 
 
+import com.am.popularmoviesstageone.BuildConfig;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -12,21 +20,41 @@ import static com.am.popularmoviesstageone.util.CONST.BASE_IMDB_URL;
 public class APIClient {
 
     private static Retrofit retrofit = null;
+    static String API_KEY = BuildConfig.API_KEY;
 
     public static Retrofit getClient() {
         if (retrofit == null) {
 
+            // For Adding The API Key to Each Request Ever Made
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            httpClient.addInterceptor(chain -> {
+                Request original = chain.request();
+                HttpUrl originalHttpUrl = original.url();
+                HttpUrl url = originalHttpUrl.newBuilder()
+                        .addQueryParameter("api_key", API_KEY)
+                        .build();
+                // Request customization: add request headers
+                Request.Builder requestBuilder = original.newBuilder()
+                        .url(url);
+                Request request = requestBuilder.build();
+                return chain.proceed(request);
+            });
+
+            // For Date Strings Format to Be Converted To Date Objects
             Gson gson = new GsonBuilder()
                     .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
                     .create();
 
+            // The Retrofit Object
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_IMDB_URL)
+                    .client(httpClient.build())
                     .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
         return retrofit;
     }
+
     public static ApiRequests getApiService() {
         return APIClient.getClient().create(ApiRequests.class);
     }
--- app/src/main/java/com/am/popularmoviesstageone/network/ApiRequests.java
+++ app/src/main/java/com/am/popularmoviesstageone/network/ApiRequests.java
@@ -19,24 +19,23 @@ import retrofit2.http.Query;
 
 
 public interface ApiRequests {
-    String API_KEY = BuildConfig.API_KEY;
 
-    @GET("movie/popular?api_key=" + API_KEY)
+    @GET("movie/popular")
     Call<MoviesList> getPopularMovies(@Query("language") String language,
                                       @Query("page") String page,
                                       @Query("region") String region);
 
-    @GET("movie/top_rated?api_key=" + API_KEY)
+    @GET("movie/top_rated")
     Call<MoviesList> getTopRatedMovies(@Query("language") String language,
                                        @Query("page") String page,
                                        @Query("region") String region);
 
-    @GET("movie/{id}/videos?api_key=" + API_KEY)
+    @GET("movie/{id}/videos")
     Call<MovieVideosEntity> getMovieTrailers(@Path("id") String movieId);
 
-    @GET("movie/{id}/reviews?api_key=" + API_KEY)
+    @GET("movie/{id}/reviews")
     Call<MovieReviewsEntity> getMovieReviews(@Path("id") String movieId);
 
-    @GET("movie/{id}?api_key=" + API_KEY)
+    @GET("movie/{id}")
     Call<MovieDetails> getMovieDetails(@Path("id") String movieId);
 }
