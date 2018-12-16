1c4097be634a6d0fb8e518f3edc6925e73c0e3b6
Abed Murad <abdallah.Murad@mail.com>
2018-12-16 04:53:27 +0100

    Add Entities to RoomDatabase

--- app/src/main/java/com/am/popularmoviesstageone/data/room/MoviesDatabase.java
+++ app/src/main/java/com/am/popularmoviesstageone/data/room/MoviesDatabase.java
@@ -5,8 +5,13 @@ import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
 
 import com.am.popularmoviesstageone.data.model.Movie;
+import com.am.popularmoviesstageone.data.model.Review;
+import com.am.popularmoviesstageone.data.model.Trailer;
 
-@Database(entities = {Movie.class}, version = 1)
+/*
+* A Good practice is to Create a Dao For Each @Entity
+* */
+@Database(entities = {Movie.class ,Review.class , Trailer.class}, version = 1)
 @TypeConverters({Converters.class})
 public abstract class MoviesDatabase extends RoomDatabase {
     public static final String NAME = "movies_database";
